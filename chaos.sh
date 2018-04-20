675a6b4d9334b9734e6f49fd22063acf55e451a1
Thomas Fan <thomasjpfan@gmail.com>
2018-04-20 05:22:43 -0400

    RFC: Less replicas

--- chaos.sh
+++ chaos.sh
@@ -5,33 +5,34 @@ trap ctrl_c INT
 monkeys=10
 
 function ctrl_c() {
-  echo "** Trapped CTRL-C"
-  for i in $(seq ${monkeys}); do
-    docker service ls |grep " monkey_${i}  " && docker service rm monkey_${i}
-  done
-  exit 0
+	echo "** Trapped CTRL-C"
+	for i in $(seq ${monkeys}); do
+		docker service ls | grep " monkey_${i}  " && docker service rm monkey_${i}
+	done
+	exit 0
 }
 
 while true; do
-  for i in $(seq ${monkeys}); do
-    case $(( ( RANDOM % 2 )  + 1 )) in
-    1)
-      echo "*** start monkey_${i}"
-      export chaos_sleep=$(( ( RANDOM % 10 )  + 1 ))
-      export chaos_start=$(( ( RANDOM % 10 )  + 1 ))
-      export chaos_service=monkey_${i}
-      export chaos_replicas=$(( ( RANDOM % 10 )  + 1 ))
-      docker service ls |grep " monkey_${i} " || ./chaos_service.sh
-      ;;
-    2) 
-      echo "*** stop monkey_${i}"
-      docker service ls |grep " monkey_${i}  " && docker service rm monkey_${i}
-      ;;
-    *)
-      echo "*** skip monkey_${i}"
-    esac
-  done
-  sleep_for=$(( ( RANDOM % 10 )  + 1 ))
-  echo "waiting for: ${sleep_for} seconds "
-  sleep ${sleep_for}
+	for i in $(seq ${monkeys}); do
+		case $(((RANDOM % 2) + 1)) in
+			1)
+				echo "*** start monkey_${i}"
+				export chaos_sleep=$(((RANDOM % 10) + 1))
+				export chaos_start=$(((RANDOM % 10) + 1))
+				export chaos_service=monkey_${i}
+				export chaos_replicas=1
+				docker service ls | grep " monkey_${i} " || ./chaos_service.sh
+				;;
+			2)
+				echo "*** stop monkey_${i}"
+				docker service ls | grep " monkey_${i}  " && docker service rm monkey_${i}
+				;;
+			*)
+				echo "*** skip monkey_${i}"
+				;;
+		esac
+	done
+	sleep_for=$(((RANDOM % 10) + 1))
+	echo "waiting for: ${sleep_for} seconds "
+	sleep ${sleep_for}
 done
